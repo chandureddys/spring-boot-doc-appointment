753ad08be378e630f88f7d524275a91c0e923e8c
chandureddys <csreddy.512@gmail.com>
2020-05-09 18:43:50 -0500

    Chandra/Venkat first commit

---
+++ src/main/java/com/petclinic/project/VeterinariansController.java
@@ -0,0 +1,20 @@
+package com.petclinic.project;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "v1/veterinarians")
+public class VeterinariansController {
+
+	@GetMapping
+	public List<String> getVeterinarians() {
+		return Arrays.asList("venkat", "chandu");
+
+	}
+
+}
---
+++ src/test/java/com/petclinic/project/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
